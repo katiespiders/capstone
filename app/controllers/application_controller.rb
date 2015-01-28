9f9674cf0f604fba864dd633c822efc3082b9aa4
Katie <kab761@gmail.com>
2015-01-27 17:27:24 -0800

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,27 @@ require 'httparty'
 
 class ApplicationController < ActionController::API
 	def index
-		url = "https://www.car2go.com/api/v2.1/vehicles?loc=seattle&oauth_consumer_key=#{ENV['CAR2GO_KEY']}&format=json"
-		cars = HTTParty.get(url)
-		render json: cars
+		render json: get_cars
 	end
+
+	private
+	
+		def origin
+			{ latitude: 47.687165,
+				longitude: -122.352925 }
+		end
+
+		def destination 
+			{ latitude: 47.606968,
+				longitude: -122.305192 }
+		end
+		
+		def get_cars
+			url = "https://www.car2go.com/api/v2.1/vehicles?loc=seattle&oauth_consumer_key=#{ENV['CAR2GO_KEY']}&format=json"
+			HTTParty.get(url)
+		end
+
+		def get_directions
+			
+		end
 end
